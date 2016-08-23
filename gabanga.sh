1d9ccb06bd21389b068caa168731566a1ea8ff16
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-23 12:11:26 +0300

    Added yet another line. It's surprised to hear that.

--- gabanga.sh
+++ gabanga.sh
@@ -5,3 +5,9 @@ unbitteble.
 I knew what it's happened. But I don't know so very fast. It's
 unbitteble.
 New life, new day. It's your life and I know be everything good.
+Yes or no. What you chouse?
+New life, new day. It's your life and I know be everything good.
+I knew what it's happened. But I don't know so very fast. It's
+Yes or no. What you chouse?
+New life, new day. It's your life and I know be everything good.
+I knew what it's happened. But I don't know so very fast. It's
