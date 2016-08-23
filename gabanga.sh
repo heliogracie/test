3a4eeab7c7b19351e64f9ca1f9a6c66df89dac1f
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-23 12:06:27 +0300

    Added another line of code.

--- gabanga.sh
+++ gabanga.sh
@@ -2,3 +2,6 @@ Yes or no. What you chouse?
 New life, new day. It's your life and I know be everything good.
 I knew what it's happened. But I don't know so very fast. It's
 unbitteble.
+I knew what it's happened. But I don't know so very fast. It's
+unbitteble.
+New life, new day. It's your life and I know be everything good.
