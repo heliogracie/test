967bce7e125ae065115712fa117397c81d6bc8b1
Helio Gracie <heliogracie@mail.ru>
2016-08-24 08:27:24 +0000

    Added new piece of code.

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@ Justa busta and stuff like that
 
 Alanah and her new boyfriend.
 I'd like to know that this happened before you born.
-<<<<<<< 7dcf6bc91ed26b900553473335035be41f655d65
 New again and again.
 And again and again.
 Rakefile new life and new days.
+I'd wanna to know is this your?
